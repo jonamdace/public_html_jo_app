36230e9b1d587f2df6925c6da82884ef0f5bdf58
Manoj <jonamdace@gmail.com>
2018-11-11 17:28:04 +0530

    bookmark

--- Platform/Templates/Bookmarked.js
+++ Platform/Templates/Bookmarked.js
@@ -10,6 +10,7 @@ import {
     ScrollView,
     Dimensions,
     TouchableOpacity,
+    ActivityIndicator,
     Image,
     ListView,
     AsyncStorage
@@ -26,13 +27,15 @@ export default class Bookmarked extends Component {
         super(props);
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
+            isLoading: true,
+            isLoadingMore: false,
             height: height,
             width: width,
             searchText: "",
             ds: ds,
             categoryId: 3,
             listItems: ds.cloneWithRows([]),
-            searchResultJson: {},
+            searchResultJson: [],
             page: "0",
             leftRecord: 0,
             previousPage: -1,
@@ -74,7 +77,12 @@ export default class Bookmarked extends Component {
     }
 
     async dataLoading() {
-        var searchResultJson = {};
+	await this.setState({isLoadingMore: true});
+
+	var stateLeftRecord = this.state.leftRecord;
+	if(this.state.isLoading || stateLeftRecord >0 ){
+
+        var searchResultJson = [];
         var that = this;
         var postJson = new FormData();
         postJson.append("page", that.state.page);
@@ -97,65 +105,141 @@ export default class Bookmarked extends Component {
             var leftRecord = parseInt(response['left_rec']);
             var nextPage = page + 1;
             var previousPage = page - 1;
-            if (searchData != null) {
-                that.updateMyState(response, 'searchResultJson');
+            if (searchData != null && searchData!=undefined && searchData!="" && searchData.length>0) {
+
                 if (leftRecord > 0)
                     that.updateMyState(nextPage, 'page');
-                that.updateMyState(previousPage, 'previousPage');
-                that.updateMyState(leftRecord, 'leftRecord');
-                that.updateMyState(nextPage, 'nextPage');
-                that.updateMyState(that.state.ds.cloneWithRows(searchData), 'listItems');
+
+		await this.setState({
+			previousPage : previousPage,
+			leftRecord : leftRecord,
+			nextPage : nextPage
+		});
+
+//                that.updateMyState(previousPage, 'previousPage');
+//                that.updateMyState(leftRecord, 'leftRecord');
+//                that.updateMyState(nextPage, 'nextPage');
+
+		if(this.state.isLoading){
+			await this.setState({
+				searchResultJson : searchData,
+				listItems : that.state.ds.cloneWithRows(searchData)
+			});
+
+			//that.updateMyState(searchData, 'searchResultJson');
+                	//that.updateMyState(that.state.ds.cloneWithRows(searchData), 'listItems');
+		} else {
+			var searchResultJson = this.state.searchResultJson;
+
+			searchData = searchResultJson.concat(searchData);
+			await this.setState({
+				searchResultJson : searchData,
+				listItems : that.state.ds.cloneWithRows(searchData)
+			});
+
+			//that.updateMyState(searchData, 'searchResultJson');
+                	//that.updateMyState(that.state.ds.cloneWithRows(searchData), 'listItems');
+		}
+		await this.setState({isLoadingMore: false});
+		await this.setState({isLoading: false});
+
             }
         }
 
+	} else {
+		await this.setState({isLoadingMore : false});
+	}
 
     }
 
     constructTemplate(item) {
+	//return <Text style={{paddingTop : 100}}>{this.state.searchResultJson.length} {JSON.stringify(item)}</Text>;
         return <SearchAdsContent imgWidth={this.state.width-50}
                                  imgHeight={150}
                                  navigation={this.navigate}
                                  postJson={item} fromPage="View My Bookmarked List" bookmarkAdd={true} dataLoading = {this.dataLoading}/>;
     }
 
-    render() {
-
-        var inputWidth = this.state.width - 30;
-        var layoutWidth = this.state.width;
-        var nextBtn = null;
-        var previousBtn = null;
-        if (this.state.leftRecord > 0) {
-            nextBtn = <TouchableOpacity onPress={()=>this.onNext()}><Text
-                style={{textAlign : 'left'}}>Next</Text></TouchableOpacity>;
+ render() {
+        if (this.state.isLoading) {
+            return (
+                <View>
+		        <View style={{height : 100}}>
+			<Navbar
+		            title={"Bookmarked"}
+		            bgColor={'orange'}
+		            left={{
+							icon: "ios-menu",
+							onPress: () => this.props.navigation.toggleDrawer(),
+						}}
+		            style={{height:60}}
+		            />
+		        </View>
+		        <View style={styles.container}>
+		            <ActivityIndicator size="large" />
+		        </View>
+                </View>
+            );
+        } else {
+            return (
+                <View>
+		        <View style={{height : 60}}>
+			<Navbar
+		            title={"View All My Ads"}
+		            bgColor={'orange'}
+		            left={{
+							icon: "ios-menu",
+							onPress: () => this.props.navigation.toggleDrawer(),
+						}}
+		            style={{height:60}}
+		            />
+		        </View>
+			<View style={{ height : this.state.height - 80, paddingBottom : 20 }}>
+                    <ListView
+                        dataSource={this.state.listItems}
+                        renderRow={(item) => this.constructTemplate(item)}
+                        onEndReached={() => this.dataLoading()}
+                        renderFooter={() => {
+            return (
+              this.state.isLoadingMore &&
+              <View style={{ flex: 1, padding: 10 }}>
+                <ActivityIndicator size="small" />
+              </View>
+            );
+          }}
+                        />
+		        </View>
+                </View>
+            );
         }
-        if (this.state.previousPage >= 0) {
-            previousBtn =
-                <TouchableOpacity onPress={()=>this.onPrevious()}><Text style={{textAlign : 'right'}}>Previous / </Text></TouchableOpacity>;
-        }
-
-        return (
-            <View style={[{height : this.state.height, flex: 1, width : layoutWidth, backgroundColor:'#59C2AF'}]}
-                  onLayout={()=> this.updateLayout()}>
-		<Navbar
-                    title={"Bookmark"}
-                    bgColor={'orange'}
-                    left={{
-						icon: "ios-menu",
-						onPress: () => this.props.navigation.toggleDrawer(),
-					}}
-                    style={{height:60}}
-                    />
-                <ScrollView >
-                    <ListView style={{paddingBottom:15}} dataSource={this.state.listItems}
-                              renderRow={(item) => this.constructTemplate(item)}
-                              enableEmptySections={true}/>
-			<MKAdsBanner />
-                    <View style={{flexDirection:"row", width : layoutWidth, paddingBottom : 20}}>
-                        <View style={ {width : layoutWidth/2}}>{ previousBtn }</View>
-                        <View style={ {width : layoutWidth/2}}>{ nextBtn }</View>
-                    </View>
-                </ScrollView>
-            </View>
-        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+    },
+    listItem: {
+        flex: 1,
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: '#d6d7da',
+        padding: 6,
+    },
+    imageWrapper: {
+        padding: 5,
+    },
+    title: {
+        fontSize: 20,
+        textAlign: 'left',
+        margin: 6,
+    },
+    subtitle: {
+        fontSize: 10,
+        textAlign: 'left',
+        margin: 6,
+    },
+});
