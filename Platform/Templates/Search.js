653d66d0a1008f41eab3b4023cd84a8c48baa7dc
Manoj <jonamdace@gmail.com>
2019-01-09 22:25:52 +0530

    search load screen

--- Platform/Templates/Search.js
+++ Platform/Templates/Search.js
@@ -1,6 +1,7 @@
 'use strict';
 import React, {Component} from "react";
 import {
+    ActivityIndicator,
     View,
     ToastAndroid,
     StyleSheet,
@@ -18,14 +19,18 @@ import {
 import SearchAdsContent from "./SearchAdsContent";
 import { doPost } from "../Component/MKActions";
 import MKAdsBanner from "../Component/MKAdsBanner";
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class Search extends Component {
+
+    static navigationOptions = ({ navigation }) => { headerRight : <Icon name="md-funnel" size={35} color={'#fff'} onPress={()=> navigation.toggleDrawer()} style={{paddingRight : 20}}/> };
 
     constructor(props:Object) {
         var {height, width} = Dimensions.get('window');
         super(props);
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
+            isLoading: true,
             height: height,
             width: width,
             searchText: "",
@@ -126,6 +131,9 @@ export default class Search extends Component {
             var nextPage = page + 1;
             var previousPage = page - 1;
             if (searchData != null) {
+                that.setState({
+                    isLoading: false,
+                })
                 that.updateMyState(response, 'searchResultJson');
                 if (leftRecord > 0)
                     that.updateMyState(nextPage, 'page');
@@ -133,6 +141,8 @@ export default class Search extends Component {
                 that.updateMyState(leftRecord, 'leftRecord');
 
 		var searchDataExist = this.state.searchData.concat(searchData)
+                searchDataExist.push({"adsId" : "ads"})
+
                 that.updateMyState(searchDataExist, 'searchData');
 
                 that.updateMyState(nextPage, 'nextPage');
@@ -145,56 +155,75 @@ export default class Search extends Component {
 
     constructTemplate(item) {
 
-        var bookmarkAdd = true;
-        var adsId = item['adsId'];
+        if(item['adsId'] != "ads"){
+            var bookmarkAdd = true;
+            var adsId = item['adsId'];
 
-       if(this.state.bookmarkArray.indexOf(adsId) == -1){
-           bookmarkAdd = false;
-       }
-        return <SearchAdsContent imgWidth={this.state.width-50}
-                                 imgHeight={150}
-                                 navigation={this.navigate}
-                                 postJson={item} fromPage="adsList" bookmarkAdd={bookmarkAdd}/>;
+            if(this.state.bookmarkArray.indexOf(adsId) == -1){
+                bookmarkAdd = false;
+            }
+            return <SearchAdsContent imgWidth={this.state.width-50}
+                                     imgHeight={150}
+                                     navigation={this.navigate}
+                                     postJson={item} fromPage="adsList" bookmarkAdd={bookmarkAdd}/>;
+        } else {
+            return <View style={{ marginTop : 10}}><MKAdsBanner /></View>
+        }
+
     }
 
 	_keyExtractor = (item, index) => item.adsId;
 
     render() {
 
-        var inputWidth = this.state.width - 30;
-        var layoutWidth = this.state.width;
-        var nextBtn = null;
-        var previousBtn = null;
-        if (this.state.leftRecord > 0) {
-            nextBtn = <TouchableOpacity onPress={()=>this.onNext()} style={{height: 30}}>
-                <Text
-                    style={[ {marginTop: 15, color:'#FFF',  fontWeight : 'bold'}]}>
-                    Load More »
-                </Text>
-            </TouchableOpacity>;
-        }
-        if (this.state.previousPage >= 0) {
-            previousBtn =
-                <TouchableOpacity onPress={()=>this.onPrevious()}><Text style={{textAlign : 'right'}}>Previous / </Text></TouchableOpacity>;
-        }
+        if (this.state.isLoading) {
+            return (
+                <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+    }}>
+                    <ActivityIndicator size="large" />
+                </View>
+            );
+        } else {
 
-        return (
-            <View style={[{height : this.state.height, flex: 1, width : layoutWidth, backgroundColor:'#59C2AF'}]}
-                  onLayout={()=> this.updateLayout()}>
-                <ScrollView>
-                <FlatList
-        data={this.state.searchData}
-        extraData={this.state}
-        keyExtractor={this._keyExtractor}
-        renderItem={(item) => this.constructTemplate(item['item'])}
-      />
-                    <View                               style={{flex : 1, width : layoutWidth, paddingBottom : 20, alignItems : "center"}}>
-                        { nextBtn }
-                    </View>
-			<MKAdsBanner />
-                </ScrollView>
+            var inputWidth = this.state.width - 30;
+            var layoutWidth = this.state.width;
+            var nextBtn = null;
+            var previousBtn = null;
+            if (this.state.leftRecord > 0) {
+                nextBtn = <TouchableOpacity onPress={()=>this.onNext()} style={{height: 30}}>
+                    <Text
+                        style={[ {marginTop: 15, color:'#FFF',  fontWeight : 'bold'}]}>
+                        Load More »
+                    </Text>
+                </TouchableOpacity>;
+            }
+            if (this.state.previousPage >= 0) {
+                previousBtn =
+                    <TouchableOpacity onPress={()=>this.onPrevious()}><Text style={{textAlign : 'right'}}>Previous / </Text></TouchableOpacity>;
+            }
 
-            </View>
-        );
+            return (
+                <View style={[{height : this.state.height, flex: 1, width : layoutWidth, backgroundColor:'#59C2AF'}]}
+                      onLayout={()=> this.updateLayout()}>
+                    <ScrollView>
+                        <FlatList
+                            data={this.state.searchData}
+                            extraData={this.state}
+                            keyExtractor={this._keyExtractor}
+                            renderItem={(item) => this.constructTemplate(item['item'])}
+                            />
+                        <View                               style={{flex : 1, width : layoutWidth, paddingBottom : 20, alignItems : "center"}}>
+                            { nextBtn }
+                        </View>
+                        <MKAdsBanner />
+                    </ScrollView>
+
+                </View>
+            );
+        }
     }
 }
